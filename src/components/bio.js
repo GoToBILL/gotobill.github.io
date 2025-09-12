0880bdffa75df7cc84257332a8b1f021cba14a6d
GoToBILL <wnqudwnrlrl@naver.com>
2025-09-12 19:54:51 +0900

    프로필 이미지를 제거했습니다

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,7 +7,6 @@
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -32,16 +31,6 @@ const Bio = () => {
 
   return (
     <div className="bio">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["auto", "webp", "avif"]}
-        src="../images/ju.jpg"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      />
       {author?.name && (
         <div>
           <p>
